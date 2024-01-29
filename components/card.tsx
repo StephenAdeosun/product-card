2c15fe85b57d747302ea96943636d668058b6ceb
StephenAdeosun <stephenadeosun411@gmail.com>
2024-01-29 22:47:40 +0100

    new

--- components/card.tsx
+++ components/card.tsx
@@ -199,6 +199,7 @@ function Card() {
               )}
             </div>
 
+
             
             {isButtonClicked && hoveredIndex === index && (
               <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-20" >
